55de4ee31a69162688f138e545a49ed717b1e734
Conrad Cardona <conradcapi@gmail.com>
2020-10-06 01:06:57 +0200

    removed login finished all

--- ASVZ_handler.py
+++ ASVZ_handler.py
@@ -1,5 +1,5 @@
-import time
-
+from datetime import datetime, timedelta
+from apscheduler.schedulers.background import BackgroundScheduler as Scheduler
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait as Wait
@@ -31,13 +31,6 @@ class ASVZ_handler(object):
             self.driver = webdriver.Firefox(options=options, executable_path=GeckoDriverManager().install())
 
     def _login(self):
-        # Wait for the event url to load
-        try:
-            Wait(self.driver, self.delay).until(EC.presence_of_element_located((By.ID, 'eventDetails')))
-            print("Event URL loaded successfully!")
-        except TimeoutException:
-            print("Event URL took too long to load.")
-
         # Initiate login
         self.driver.find_elements_by_tag_name('button')[1].click()
         
@@ -87,12 +80,7 @@ class ASVZ_handler(object):
         except TimeoutException:
             print("Login took too long.")
         
-
-    def enrol(self, url):
-        self.driver.get(url)
-        # TODO: Log in only 20ish minutes before enrolment
-        self._login()
-        # TODO: refresh one millisecond after enrolment
+    def _enrol(self):
         self.driver.refresh()
 
         # Wait for the refresh to take place
@@ -119,8 +107,42 @@ class ASVZ_handler(object):
         except TimeoutException:
             print("Enrolment took too long.")
 
+    def _get_enrolment_time(self):
+        for elem in self.driver.find_elements_by_tag_name('dd'):
+            if elem.text.count(',') == 2:
+                date = elem.text.split('-')[0][4:-1]
+        parsed_date = datetime.strptime(date, '%d.%m.%Y %H:%M')
+        return parsed_date
+
+    def enrol(self, url):
+        # Get the Event URL
+        self.driver.get(url)
+
+       # Wait for the event url to load
+        try:
+            Wait(self.driver, self.delay).until(EC.presence_of_element_located((By.ID, 'eventDetails')))
+            print("Event URL loaded successfully!")
+        except TimeoutException:
+            print("Event URL took too long to load.")
+
+        # Get enrolment time and login time (1 min before enrolment)
+        enrolment_time = self._get_enrolment_time() + timedelta(milliseconds=10)
+        login_time = enrolment_time - timedelta(minutes=1)
+
+        # Setup the scheduler with both jobs
+        scheduler = Scheduler()
+        scheduler.add_job(self._login, 'date',run_date=login_time)
+        scheduler.add_job(self._enrol, 'date',run_date=enrolment_time)
+        scheduler.start()
+        
+        while scheduler.get_jobs():
+            pass
+        scheduler.shutdown()
+    
+ 
+
  
 if __name__ == '__main__':
     # Enter your login credentials here
-    fb_login = ASVZ_handler(login='ccardona', password='HQaF6j3E6mqtrad')
-    fb_login.enrol('https://schalter.asvz.ch/tn/lessons/139683')
+    asvz = ASVZ_handler(login='', password='')
+    asvz.enrol('https://schalter.asvz.ch/tn/lessons/139683')
